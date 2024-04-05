54f0a2f9a27d7411a1fc2e939fdaca2173547537
BetoNajera <ing.betonajera@gmail.com>
2024-04-04 18:28:10 -0600

    :memo: Doc: Add comments
    Add comments

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -2,23 +2,39 @@ import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDTO {
-  @ApiProperty()
+  /**
+   * The name of the user
+   * @example Brad
+   */
+  @ApiProperty({ description: 'The name of the user' })
   @IsNotEmpty()
   @IsString()
   name: string;
 
-  @ApiProperty()
+  /**
+   * The last name of the user
+   * @example Torp
+   */
+  @ApiProperty({ description: 'last name of the user' })
   @IsNotEmpty()
   @IsString()
   lastName: string;
 
-  @ApiProperty()
+  /**
+   * The email of the user
+   * @example example@email.com
+   */
+  @ApiProperty({ description: 'The email of the user' })
   @IsNotEmpty()
   @IsString()
   @IsEmail()
   email: string;
 
-  @ApiProperty()
+  /**
+   * The password of the user
+   * @example 1q2w3e4r
+   */
+  @ApiProperty({ description: 'The password of the user' })
   @IsNotEmpty()
   @IsString()
   password: string;
--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -2,19 +2,31 @@ import { IsString, IsEmail, IsOptional, ValidateIf } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UserDTO {
+  /**
+   * The name of the user
+   * @example Brad
+   */
   @ValidateIf((dto) => typeof dto.lastName === 'undefined')
-  @ApiProperty()
+  @ApiProperty({ description: 'The name of the user' })
   @IsOptional()
   @IsString()
   name?: string;
 
+  /**
+   * The last name of the user
+   * @example Torp
+   */
+  @ApiProperty({ description: 'last name of the user' })
   @ValidateIf((dto) => typeof dto.email === 'undefined')
-  @ApiProperty()
   @IsOptional()
   @IsString()
   lastName?: string;
 
-  @ApiProperty()
+  /**
+   * The email of the user
+   * @example example@email.com
+   */
+  @ApiProperty({ description: 'The email of the user' })
   @IsOptional()
   @IsString()
   @IsEmail()
